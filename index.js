985ed413a2766206402453bd353293bcf16a0be7
Llitvinchuk <username@example.com>
2022-11-03 22:45:23 +0300

    all

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import { initialCards } from "./data.js";
 let popupContainer = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
@@ -5,20 +6,21 @@ let formaContainer =  document.querySelector('.forma');
 let addButton = document.querySelector('.profile__add-button')
 let closeButtonForma = document.querySelector('.forma__close-button');
 let addCards = document.querySelector('.forma__create-button')
-let cardContainer = document.querySelector('.elements')
+let cardsContainer = document.querySelector('.element-container')
+let closePlace = document.querySelector('.place__close')
 
 function activePopup() {
-    popupContainer.classList.remove('popup_disabled'); 
+    popupContainer.classList.add('popup_active'); 
 }
 function disabledPopup() {
-    popupContainer.classList.add('popup_disabled'); 
+    popupContainer.classList.remove('popup_active'); 
 }
 function activeForma() {
-    formaContainer.classList.remove('forma_disabled'); 
+    formaContainer.classList.add('forma_active'); 
 
 }
 function disabledForma() {
-    formaContainer.classList.add('forma_disabled'); 
+    formaContainer.classList.remove('forma_active'); 
 }
 editButton.addEventListener('click', function () {
     activePopup()
@@ -32,6 +34,14 @@ closeButton.addEventListener('click', function () {
 closeButtonForma.addEventListener('click', function () {
     disabledForma()
   });
+  addCards.addEventListener(`click`, function() {
+    addnewCard()
+  })
+closePlace.addEventListener('click', function () {
+  let place = document.querySelector('.place');
+  place.classList.remove(`place_active`)
+  place.classList.add(`place_disabled`)
+})
   // Находим форму в DOM
 const formElement = document.querySelector('.popup__form')// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
@@ -56,40 +66,61 @@ function formSubmitHandler (evt) {
     disabledPopup()
     // Вставьте новые значения с помощью textContent
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+function addnewCard(titleValue, imageValue) {
+  const elementsTemplate = document.querySelector('#template-elements').content;
+  const cardElement = elementsTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__title').textContent = titleValue;
 
+  cardElement.querySelector('.element__image').src = imageValue;
+  cardElement.querySelector('.element__image').addEventListener(`click`, function (evt) {
+    let place = document.querySelector('.place')
+    place.classList.add('place_active')
+    place.querySelector(`.place__image`).src = evt.target.src
 
-  initialCards.forEach(function (element) {
-    let cardElement = document.querySelector('.element').content;
-    const cardTemplate = cardElement.querySelector('').cloneNode(true);
-    console.log('hghy')
-    cardTemplate.querySelector('.element__image').src = element.src;
-    cardTemplate.querySelector('.element__title').textContent = element.title;
-    cardTemplate.querySelector('.element__button').addEventListener('click', function (evt) {
-            evt.target.classList.toggle('.element__button_active');
-    });
-    cardTemplate.querySelector('.element__trash').addEventListener('click', function () {
-        cardTemplate.remove()
-    });
-    cardContainer.prepend(cardTemplate);
-  });
+    place.querySelector('.place__text').innerHTML = titleValue;
+  })
+  cardElement.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
 
-//   function addCard(imageValue, titleValue) {
-//     const cardElement = document.querySelector('.element').cloneNode(true);
-//     cardElement.querySelector('.element__image').src = imageValue;
-//     cardElement.querySelector('.element__title').textContent = titleValue;
-//     cardElement.querySelector('.element__button').addEventListener('click', function (evt) {
-//       evt.target.classList.toggle('.element__button_active');
-//   });
+}); 
+cardElement.querySelector('.element__trash').addEventListener('click', function () {
+  cardElement.remove();
 
-//     return cardElement;
+}); 
 
-//   }
-// addCards.addEventListener('click', () => {
-
-//     addCard(imageValue, titleValue);
-//     cardContainer.prepend(addCard(cardElement));
-//     });
+  cardsContainer.prepend(cardElement)
+  disabledForma();
+  // добавьте songElement название песни
+}
+for (let i = 0;i < initialCards.length; i++) {
+  addnewCard(initialCards[i].name, initialCards[i].link)
+}
